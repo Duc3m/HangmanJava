8214559deb9cdfe23de6de02056364b979289ca5
Đức Em <ducemtran44@gmail.com>
2024-07-20 20:38:51 +0700

    initialize

---
+++ src/hangmanjava/HangmanJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hangmanjava;
+
+/**
+ *
+ * @author Duc3m
+ */
+public class HangmanJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
